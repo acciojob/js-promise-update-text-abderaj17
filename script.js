c88ff3333375bdd8f035218da7ce1822c90925a2
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-17 19:09:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const output  = document.getElementById('output');
+
+document.addEventListener('click', ()=>{
+	const fun = new Promise(()=>{
+		setTimeout(()=>{
+			output.innerHTML = `<h1>Hello, world!</h1>`;
+		}, 1000)
+	})
+})
